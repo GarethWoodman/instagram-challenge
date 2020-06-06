1d683b8808ee60fae5557f45fb0bcf72e0715d31
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-06 18:36:40 +0100

    Successfully tested User model

--- instagram_clone/app/models/user.rb
+++ instagram_clone/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_presence_of :username, :email, :password
+  validates_uniqueness_of :username, :email
+  validates_length_of :username, :maximum => 20
+  validates_length_of :password, :minimum => 6
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- instagram_clone/spec/models/user_spec.rb
+++ instagram_clone/spec/models/user_spec.rb
@@ -1,17 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:valid_username)    { 'gwoodman92' }
+  let(:invalid_username)  { 'gwoodman92gwoodman92gwoodman92gwoodman92' }
+
+  let(:valid_email)       { 'gwoodman92@example.com' }
+  let(:invalid_email)     { 'gwoodman-at.com' }
+
+  let(:valid_password)    { '123456' }
+  let(:invalid_password)  { '123' }
+
+  def create_user(username, email, password, password_confirmation=valid_password)
+    User.create(username: username, email: email, password: password, password_confirmation: password_confirmation)
+  end
+
+  let(:valid_user) do
+    create_user(valid_username, valid_email, valid_password, valid_password)
+  end
 
   describe "If User is valid then" do
-    let(:valid_user_account) do
-      username = 'gwoodman92'
-      email = 'gwoodman92@example.com'
-      password = '123456'
-      User.create(username: username, email: email, password: password, password_confirmation: password)
+    it "has username" do
+      expect(valid_user.username).to eq 'gwoodman92'
     end
 
-    it "has username" do
-      expect(valid_user_account.username).to eq 'gwoodman92'
+    it "has email" do
+      expect(valid_user.email).to eq 'gwoodman92@example.com'
+    end
+
+    it "has encryped password" do
+      expect(valid_user.authenticate('123456')).to_not eq false
+    end
+  end
+
+  describe "If User attribute is invalid" do
+    it "username is invalid" do
+
+      user_with_invalid_username = create_user(invalid_username, valid_email, valid_password)
+      expect(user_with_invalid_username.id).to eq nil
+    end
+
+    it "email is invalid" do
+      user_with_invalid_email = create_user(valid_username, invalid_email, valid_password)
+      expect(user_with_invalid_email.id).to eq nil
+    end
+
+    it "password is invalid" do
+      user_with_invalid_password = create_user(invalid_username, valid_email, invalid_password, invalid_password)
+      expect(user_with_invalid_password.id).to eq nil
+    end
+  end
+
+  describe "If User enters incorrect password" do
+    it "password_confirmation does not match password" do
+      user_with_invalid_password_confirmation = create_user(valid_username, valid_email, valid_password, 'adifferentpassword')
+      expect(user_with_invalid_password_confirmation.id).to eq nil
+    end
+
+    it "password to login is incorrect" do
+      expect(valid_user.authenticate('123498')).to eq false
     end
   end
 
